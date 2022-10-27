23ab66b819eac2a2810fe11ac46a82de8a53aa23
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-10-27 19:40:39 -0300

    screenShotHandler

---
+++ SeleniumProject/Handler/ScreenShotHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
+using SeleniumProject.PageObject;
+
+
+namespace SeleniumProject.Handler;
+
+public class ScreenShotHandler
+{
+    // Method to obtain the directory location where we're going to save the image
+    private static string DirectoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+    // Method to take screenschot with Selenium
+    // It returns the image path taken
+    public static string TakeScreenShot(IWebDriver driver)
+    {
+        long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+        string imagePath = DirectoryPath + "//img." + milliseconds + ".png";
+        Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+        image.SaveAsFile(imagePath, ScreenshotImageFormat.Png);
+
+        return imagePath;
+    }
+}
+
--- TestCases/BaseTest.cs
+++ TestCases/BaseTest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Configuration;
 using SeleniumProject.PageObject;
+using SeleniumProject.Handler;
 
 namespace TestCases;
 
@@ -12,12 +15,15 @@ public abstract class BaseTest
     // Selenium Driver
     protected IWebDriver Driver;
 
+    protected string url = ConfigurationManager.AppSettings["url"];
+
     // SetUp: NUnit notation to execute a method before each test.
     // Method to initiate Chrome and navigate to an url
     [SetUp]
     public void BeforeBaseTest()
     {
         Driver = new ChromeDriver();
+        // CAMBIAR PARAMETRO POR VARIABLE URL
         Driver.Navigate().GoToUrl("https://www.testfaceclub.com/ejercicios/");
     }
 
@@ -26,6 +32,11 @@ public abstract class BaseTest
     [TearDown]
     public void AfterBaseTest()
     {
+        var status = TestContext.CurrentContext.Result.Outcome.Status;
+
+        if (status == TestStatus.Failed)
+            ScreenShotHandler.TakeScreenShot(Driver);
+
         if (Driver != null)
         {
             Driver.Quit();
