32b9fcf62c81329fbbd167a47bd7dc77d6b20a53
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-10-27 12:21:42 -0300

    testCase/login

--- SeleniumProject/PageObject/EmployeePage.cs
+++ SeleniumProject/PageObject/EmployeePage.cs
@@ -21,12 +21,28 @@ public class EmployeePage
     // Localizadores
 
     protected By Form = By.Id("formEmployee");
+    protected By NameInput = By.XPath("//*[@id=\"formEmployee\"]/div[2]/div[1]/input");
+    protected By EmailInput = By.XPath("//*[@id=\"formEmployee\"]/div[2]/div[2]/input");
+    protected By AddressTextArea = By.Id("address");
+    protected By PhoneInput = By.Id("phone");
+    protected By AddButton = By.Id("addButton");
 
     // Method to detect if the employee form is loaded. Returns true if the element is present; otherwise returns false.
     public bool FormIsPresent()
     {
         return WaitHandler.ElementIsPresent(Driver, Form);
     }
+
+    public void AddEmployee(string name, string email, string address, string phone)
+    {
+        Driver.FindElement(NameInput).SendKeys(name);
+        Driver.FindElement(EmailInput).SendKeys(email);
+        Driver.FindElement(AddressTextArea).SendKeys(address);
+        Driver.FindElement(PhoneInput).SendKeys(phone);
+        Driver.FindElement(AddButton).Click();
+    }
+
+    public bool
 
 }
 
---
+++ TestCases/LoginTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumProject.PageObject;
+
+
+namespace TestCases
+{
+    // NUnit notation to mark a class that contains test cases
+    [TestFixture]
+    public class LoginTest
+    {
+
+        // Selenium Driver
+        protected IWebDriver Driver;
+
+        // SetUp: NUnit notation to execute a method before each test.
+        // Method to initiate Chrome and navigate to an url
+        [SetUp]
+        public void BeforeTest()
+        {
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("https://www.testfaceclub.com/ejercicios/");
+        }
+
+        // Test: NUnit notation to mark a method as an automated test case
+        // Method that implements Login test case. The expected result is that the user logs in.
+        [Test]
+        public void SuccesfulLoginTest()
+        {
+            LoginPage loginPage = new LoginPage(Driver);
+            EmployeePage employeePage = loginPage.LogInAs("admin", "123");
+
+            Assert.IsTrue(employeePage.FormIsPresent());
+        }
+
+        // TearDown: NUnit notation to execute a method after each test.
+        // Method to close the navigator.
+        [TearDown]
+        public void AfterTest()
+        {
+            if (Driver != null)
+            {
+                Driver.Quit();
+            }
+        }
+
+    }
+}
